132d735cc24e35194f6be662cadc5d17d030ded4
Mark <markhenningerwork@gmail.com>
2017-04-25 18:38:12 +0200

    add code to fetch html data and clean up with beautiful soup

--- fetch_data.py
+++ fetch_data.py
@@ -1,1 +1,47 @@
-fetch_data.py
+
+#use the first.py programm to pull data from project gutenberg
+
+
+"""
+Help to get data from project gutenberg
+
+import urllib2
+
+response = urllib2.urlopen("http://www.gutenberg.org/files/135/135-h/135-h.htm")
+html = response.read()
+
+sad = 0
+
+list_of_words = html.split(' ')
+
+for word in list_of_words:
+	if word == "sad":
+		sad += 1
+
+print sad"""
+
+import urllib2
+from bs4 import BeautifulSoup
+
+
+#function to fetch data
+def fetch_data(url):
+	response = urllib2.urlopen(url)
+	html = response.read()
+	soup = BeautifulSoup(html, 'html.parser')
+	clean_soup = (soup.get_text())
+
+
+#add to code that you only get the stuff between PREFACE and End of Project Gutenberg's The Sorrows of Young Werther, by J.W. von Goethe
+	return clean_soup
+
+
+
+#function to get rid of html code etc
+def clean_code():
+	return
+
+url = "http://www.gutenberg.org/files/2527/2527-h/2527-h.htm"
+
+print fetch_data(url)
+
--- run.py
+++ run.py
@@ -4,12 +4,12 @@ It is about building a markov chain generator"""
 """## How to use the Markov Chain text generator
 1. After importing this module into your main project script, create an instance of MarkovChain and assign it to a variable. For example `mc = MarkovChain()`
 2. Use one of the methods to read a local text file or a string. You can call this method multiple times to add additional data.
-3. Call the `generate_text()` function on the instance of MarkovChain you created to generate text from the Markov Chain. 
-You can call this method multiple times to generate additional data. This function will output a list of words. If your project requires a different format, you should convert the output accordingly."""
+3. Call the `generate_text()` function on the instance of MarkovChain you created to generate text from the Markov Chain. You can call this method multiple times to generate additional data. This function will output a list of words. If your project requires a different format, you should convert the output accordingly."""
 
 
 #need to import markov chain class
 from markov_python.cc_markov import MarkovChain
+import fetch_data
 
 mc = MarkovChain()
 
@@ -18,3 +18,26 @@ mc = MarkovChain()
 mc.add_string("test test test test eibs zwo drei vier funf.")
 print mc.generate_text()
 print mc.generate_text()
+
+
+
+"""Planning of the prgramm parts
+
+1) input which book they want an excerpt from
+1.1) how many excerpts
+2) fetch data from that book
+3) generate markov chain
+4) get data from chain based on how many they want
+5) present the data formated
+
+"""
+
+#this is the starter function
+def start_here():
+	return
+
+#function to format data
+def format_text():
+	return
+
+
