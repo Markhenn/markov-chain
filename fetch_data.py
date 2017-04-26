4bae53e79f3b0e93f1bb877778339d29bd894731
Mark <markhenningerwork@gmail.com>
2017-04-26 07:12:37 +0200

    cut story to important part

--- fetch_data.py
+++ fetch_data.py
@@ -26,14 +26,21 @@ from bs4 import BeautifulSoup
 
 #function to fetch data
 def fetch_data(url):
+	#get html from gutenberg
 	response = urllib2.urlopen(url)
 	html = response.read()
+	#clean up text
 	soup = BeautifulSoup(html, 'html.parser')
 	clean_soup = (soup.get_text())
+	#cut text to only the story
+	start_index = clean_soup.find("I have carefully")
+	end_index = clean_soup.find("End of Project")
+	story = clean_soup[int(start_index):int(end_index)]
 
 
-#add to code that you only get the stuff between PREFACE and End of Project Gutenberg's The Sorrows of Young Werther, by J.W. von Goethe
-	return clean_soup
+
+#add to code that you only get the stuff between PREFACE and End of Project Gutenberg's The Sorrows of Young Werther, by J.W. von Goethe - num_preface = soup.find_all(string="Werther")
+	return story
 
 
 
