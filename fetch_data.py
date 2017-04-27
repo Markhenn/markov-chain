7bf7194e9bb680c8238b8972b0b53abbd3deb791
Mark <markhenningerwork@gmail.com>
2017-04-27 07:06:31 +0200

    finish fetch data and import to run.py - update outline

--- fetch_data.py
+++ fetch_data.py
@@ -1,24 +1,6 @@
 
-#use the first.py programm to pull data from project gutenberg
+#This Program fetches data from a web page and displays just text
 
-
-"""
-Help to get data from project gutenberg
-
-import urllib2
-
-response = urllib2.urlopen("http://www.gutenberg.org/files/135/135-h/135-h.htm")
-html = response.read()
-
-sad = 0
-
-list_of_words = html.split(' ')
-
-for word in list_of_words:
-	if word == "sad":
-		sad += 1
-
-print sad"""
 
 import urllib2
 from bs4 import BeautifulSoup
@@ -29,6 +11,12 @@ def fetch_data(url):
 	#get html from gutenberg
 	response = urllib2.urlopen(url)
 	html = response.read()
+	return clean_code(html)
+
+
+
+#function to get rid of html code etc
+def clean_code(html):
 	#clean up text
 	soup = BeautifulSoup(html, 'html.parser')
 	clean_soup = (soup.get_text())
@@ -36,19 +24,7 @@ def fetch_data(url):
 	start_index = clean_soup.find("I have carefully")
 	end_index = clean_soup.find("End of Project")
 	story = clean_soup[int(start_index):int(end_index)]
-
-
-
-#add to code that you only get the stuff between PREFACE and End of Project Gutenberg's The Sorrows of Young Werther, by J.W. von Goethe - num_preface = soup.find_all(string="Werther")
 	return story
 
 
-
-#function to get rid of html code etc
-def clean_code():
-	return
-
-url = "http://www.gutenberg.org/files/2527/2527-h/2527-h.htm"
-
-print fetch_data(url)
 
--- run.py
+++ run.py
@@ -7,30 +7,23 @@ It is about building a markov chain generator"""
 3. Call the `generate_text()` function on the instance of MarkovChain you created to generate text from the Markov Chain. You can call this method multiple times to generate additional data. This function will output a list of words. If your project requires a different format, you should convert the output accordingly."""
 
 
-#need to import markov chain class
-from markov_python.cc_markov import MarkovChain
-import fetch_data
-
-mc = MarkovChain()
-
-
-# RnD a string from the internet to add here
-mc.add_string("test test test test eibs zwo drei vier funf.")
-print mc.generate_text()
-print mc.generate_text()
-
-
-
 """Planning of the prgramm parts
 
-1) input which book they want an excerpt from
-1.1) how many excerpts
+1) how many excerpts
 2) fetch data from that book
 3) generate markov chain
 4) get data from chain based on how many they want
 5) present the data formated
 
 """
+
+
+#need to import markov chain class
+from markov_python.cc_markov import MarkovChain
+import fetch_data
+
+mc = MarkovChain()
+
 
 #this is the starter function
 def start_here():
@@ -41,3 +34,13 @@ def format_text():
 	return
 
 
+#create the raw text from project gutenberg
+url = "http://www.gutenberg.org/files/2527/2527-h/2527-h.htm"
+raw_text = fetch_data.fetch_data(url)
+
+
+
+# RnD a string from the internet to add here
+mc.add_string(raw_text)
+print mc.generate_text()
+print mc.generate_text()
